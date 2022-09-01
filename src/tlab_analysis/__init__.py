f90a41da61dbf88827ca2002d1a29285e9a1bb49
Shuhei Nitta <huisintheta@gmail.com>
2022-09-01 02:56:44 +0000

    🔖 Release v0.0.2

--- src/tlab_analysis/__init__.py
+++ src/tlab_analysis/__init__.py
@@ -1,9 +1,5 @@
-__version__ = "0.0.1"
-
+__version__ = "0.0.2"
 
 from .abstract import AbstractData as AbstractData
-from .photo_luminescence import Data as PLData
-from .photo_luminescence import TimeResolved as PLTimeResolved
-from .photo_luminescence import WavelengthResolved as PLWavelengthResolved
 from .utils import determine_fit_range_dc as determine_fit_range_dc
 from .utils import find_scdc as find_scdc
