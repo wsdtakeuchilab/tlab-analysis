cf3f0644f400954c31466c6bb493baa157140748
Shuhei Nitta <huisintheta@gmail.com>
2022-11-10 20:19:49 +0000

    Add conftest.py

---
+++ src/conftest.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+from tlab_analysis import trpl
+
+
+@pytest.fixture()
+def trpl_data() -> trpl.TRPLData:
+    """
+    For tlab_analysis.trpl.TRPLData.
+    """
+    np.random.seed(0)
+    time = np.linspace(0, 10, 3, dtype=np.float32)
+    wavelength = np.linspace(400, 500, 3, dtype=np.float32)
+    intensity = np.random.randint(0, 100, len(time) * len(wavelength))
+    data = trpl.TRPLData(
+        pd.DataFrame(
+            dict(
+                time=np.repeat(time, len(wavelength)),
+                wavelength=np.tile(wavelength, len(time)),
+                intensity=intensity,
+            )
+        )
+    )
+    return data
