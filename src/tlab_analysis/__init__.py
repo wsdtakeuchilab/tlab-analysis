f3fa5d5707197a5832238d50f35fa262c9a844b9
Shuhei Nitta <huisintheta@gmail.com>
2023-12-08 03:29:16 +0000

    Update tlab_analysis

--- src/tlab_analysis/__init__.py
+++ src/tlab_analysis/__init__.py
@@ -1,6 +1,7 @@
 __version__ = "0.4.1"
 
 from . import abstract as abstract
+from . import apparatus as apparatus
 from . import cwpl as cwpl
 from . import trpl as trpl
 from . import utils as utils
