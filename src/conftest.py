8b580cb672672d0a36f24a7511bf405fc43e2b4c
Shuhei Nitta <huisintheta@gmail.com>
2023-12-08 00:55:28 +0000

    Update conftest.py

--- src/conftest.py
+++ src/conftest.py
@@ -12,7 +12,7 @@ def trpl_data() -> trpl.TRPLData:
     """
     np.random.seed(0)
     time = np.linspace(0, 10, 3, dtype=np.float32)
-    wavelength = np.linspace(400, 500, 3, dtype=np.float32)
+    wavelength = np.linspace(200, 400, 3, dtype=np.float32)
     intensity = np.random.randint(0, 100, len(time) * len(wavelength))
     data = trpl.TRPLData(
         pd.DataFrame(
