4e110814324d3cc991befc2f4165cbfe20dc76df
Shuhei Nitta <huisintheta@gmail.com>
2024-01-10 21:57:35 +0000

    Update test_cwpl.py

--- tests/test_cwpl.py
+++ tests/test_cwpl.py
@@ -18,7 +18,7 @@ def data(request: FixtureRequest[int]) -> cwpl.CWPLData:
         "0\r\n",
         "0\r\n",
         "0\r\n",
-        '"                                                                                                                                                                                                                                                                "\r\n',
+        '"                                                                                                                                                                                                                                                                "\r\n',  # noqa: E501
         "501\r\n",
         '"no data"\r\n',
         '"no data"\r\n',
